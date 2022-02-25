73c8128a32fef74cec16d1f99290f09494cbfcf0
Pham Dat <phamtuandat1a0@gmail.com>
2022-02-25 19:52:15 +0700

    change rule of image field

--- models/User-models.js
+++ models/User-models.js
@@ -8,7 +8,7 @@ const userSchema = new Schema(
         name: { type: String, required: true },
         email: { type: String, required: true, unique: true },
         password: { type: String, required: true, minlength: 6 },
-        image: { type: String, required: true },
+        image: { type: String, required: false },
         rooms: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Room', unique: true }],
         connections: [{ type: mongoose.Types.ObjectId, required: true, ref: 'User' }],
     },
